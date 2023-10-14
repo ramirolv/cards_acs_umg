0f4cd68d00fcf770ce15a2a4c3ccccab7f41c8b4
Ramiro López <roveltaim1973@gmail.com>
2023-10-14 00:03:00 -0600

    feat: lista de cards y uso de archivos media

--- cards_umg/apps/core/urls.py
+++ cards_umg/apps/core/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from .views import *
 
 app_name='core'
 
 urlpatterns = [
-    path('', home_view, name='nueva'),
+    path('', home_view, name='home'),
     path('categorias/', categorias_view, name='categorias'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
