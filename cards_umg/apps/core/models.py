c721847cc4e9e1ed8a6a7a39c9dec59feec8c49a
Ramiro López <roveltaim1973@gmail.com>
2023-10-13 23:16:45 -0600

    feat: creacion de signals al modelo categoria.

--- cards_umg/apps/core/models.py
+++ cards_umg/apps/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -9,3 +11,26 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+
+# Definir una función para eliminar la imagen asociada a la categoría antes de que se elimine la categoría
+@receiver(pre_delete, sender=Categoria)
+def eliminar_imagen_categoria(sender, instance, **kwargs):
+    # Asegúrate de que haya una imagen asociada antes de intentar eliminarla
+    if instance.icono:
+        instance.icono.delete()
+
+# Definir una función para eliminar la imagen anterior si se cambia por otra
+@receiver(pre_save, sender=Categoria)
+def eliminar_imagen_anterior(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+
+    try:
+        # Recupera la categoría existente
+        categoria_existente = Categoria.objects.get(pk=instance.pk)
+        # Comprueba si la imagen se ha cambiado
+        if categoria_existente.icono and categoria_existente.icono != instance.icono:
+            # Elimina la imagen anterior
+            categoria_existente.icono.delete()
+    except Categoria.DoesNotExist:
+        return False
